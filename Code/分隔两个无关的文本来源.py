9d37d720ac76cf58b0a00990dbe3a0999442f7f3
Chenye-yang <chye.y000@gmail.com>
2025-03-27 23:16:25 +0800

    modified 分隔两个无关的文本来源.py in Code

---
+++ Code/分隔两个无关的文本来源.py
@@ -0,0 +1,57 @@
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab #A
+        self.int_to_str = {i:s for s,i in vocab.items()} #B
+    def encode(self, text): #C
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    def decode(self, ids): #D
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) #E
+        return text 
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])
+
+
+import re
+text = "Hello, world. This, is a test."
+result = re.split(r'(\s)', text)
+print(result)
+
+result = re.split(r'([,.]|\s)', text)
+print(result)
+
+preprocessed = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip]
+print(len(preprocessed))
+
+all_words = sorted(list(set(preprocessed)))
+vocab_size = len(all_words)
+print(vocab_size)
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i > 50:
+        break
+
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = "It's the last he painted, you know"
+ids = tokenizer.encode(text)
+print(ids)
+
+print(tokenizer.decode(ids))
+
+# text = "Hello, do you like tea?"
+# tokenizer.encode(text)
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+print(len(vocab.items()))
