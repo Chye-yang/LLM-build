92a88314db1efd39e40ec0d027cef0d1d119aae8
Chenye-yang <chye.y000@gmail.com>
2025-03-31 10:53:08 +0800

    modified 分隔两个无关的文本来源.py in Code

--- Code/分隔两个无关的文本来源.py
+++ Code/分隔两个无关的文本来源.py
@@ -1,16 +1,19 @@
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab #A
         self.int_to_str = {i:s for s,i in vocab.items()} #B
-    def encode(self, text): #C
+    def encode(self, text):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item.strip() for item in preprocessed if item]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
-    def decode(self, ids): #D
+    def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) #E
-        return text 
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
@@ -41,15 +44,15 @@ for i, item in enumerate(vocab.items()):
         break
 
 
-tokenizer = SimpleTokenizerV1(vocab)
-text = "It's the last he painted, you know"
-ids = tokenizer.encode(text)
-print(ids)
+# tokenizer = SimpleTokenizerV1(vocab)
+# text = "It's the last he painted, you know"
+# ids = tokenizer.encode(text)
+# print(ids)
 
-print(tokenizer.decode(ids))
+# print(tokenizer.decode(ids))
 
-# text = "Hello, do you like tea?"
-# tokenizer.encode(text)
+# # text = "Hello, do you like tea?"
+# # tokenizer.encode(text)
 
 all_tokens = sorted(list(set(preprocessed)))
 all_tokens.extend(["<|endoftext|>", "<|unk|>"])
@@ -58,3 +61,12 @@ print(len(vocab.items()))
 for i, item in enumerate(list(vocab.items())[-5:]):
     print(item)
 
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+
+print(tokenizer.decode(tokenizer.encode(text)))
